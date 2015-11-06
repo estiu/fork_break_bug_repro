9341efc367241bbb8175a7472ea12eaede83ce54
vemv <vemv@vemv.net>
2015-11-06 06:00:41 +0100

    Create repro code

--- app/models/foo.rb
+++ app/models/foo.rb
@@ -1,2 +1,16 @@
 class Foo < ActiveRecord::Base
+  
+  include ForkBreak::Breakpoints
+  
+  def repro
+    breakpoints << :transaction
+  end
+
+  def self.demo
+    process1 = ForkBreak::Process.new {
+      Foo.new.repro
+    }
+    process1.run_until(:transaction).wait
+  end
+    
 end
